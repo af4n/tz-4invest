0b2ca0a0fe079423c419ce10f4982c27feb49d22
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2015-11-16 11:43:25 +0200

    init responsive

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$(".btn-user-xs").click(function() {
+		$(".btn-user").slideToggle(),
+		$(".btn-reg").slideToggle();
+	});
+
+	$(".btn-nav").click(function() {
+		$(".topline-nav ul").slideToggle();
+	});
+
+
+});
+
